f6032b0c602a6d4e8ec6a3346bff5556efd4bdd1
John Magnuson <sobewon@gmail.com>
2023-04-04 14:28:57 -0400

    fixed console log overwrite + updated add employee

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const cTable = require('console.table');
 const inquirer = require('inquirer');
+const { error } = require('console');
 require('dotenv').config();
 
 //const PORT = process.env.PORT || 3001;
@@ -18,18 +19,19 @@ const db = mysql.createConnection(
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME
     },
-    console.log(`DB connected`)
 );
 
 //test to select all from employee
+/*
 db.query('SELECT * FROM employee', (error, results) => {
     if (error) throw error;
     console.log(`\n`)
     console.table(results);
-});
+}); */
 
 async function promptUser() {
-    while (true) {
+    let isRun = true;
+    while (isRun) {
         const answer = await inquirer.prompt([
             {
             type: 'list',
@@ -46,46 +48,148 @@ async function promptUser() {
             }
         ])
         switch (answer.option) {
-            case 'View All Employees':
+            case 'View All Employees': //finished
                 console.log('\ncode for view all empl')
-                db.query('SELECT * FROM employee', (error, results) => {
-                    if (error) throw error;
-                    console.log('\n');
+                try {
+                    const results = await new Promise((resolve, reject) => {
+                        db.query('SELECT * FROM employee', (error, results) => {
+                            if (error) reject(error);
+                            resolve(results);
+                        });
+                    });
+                    console.log('\n Printing Table: \n');
                     console.table(results);
-                });
+                    console.log(`\n`);
+                } catch (error) {
+                    console.log('Error:', error);
+                }
                 break;
-            case 'Add Employee':
-                console.log('\ncode for adding empl')
+            case 'Add Employee': //fin
+                try{
+                    console.log('\nAdding Employee\n')
+                    const employee = await inquirer.prompt([
+                        {
+                            type: 'input',
+                            name: 'first_name',
+                            message: 'Enter the first name of the employee:'
+                        },
+                        {
+                            type: 'input',
+                            name: 'last_name',
+                            message: 'Enter the last name of the employee:'
+                        },
+                        {
+                            type: 'input',
+                            name: 'role_id',
+                            message: 'Enter the role ID of the employee:'
+                        },
+                        {
+                            type: 'input',
+                            name: 'manager_id',
+                            message: 'Enter the manager ID of the employee:'
+                        }
+                    ]);
+                    // insert new emp
+                    await new Promise((resolve, reject) => {
+                        const dbQuery = `
+                        INSERT INTO employee (first_name, last_name, role_id, manager_id)
+                        VALUES (?, ?, ?, ?)
+                        `;
+                        db.query(dbQuery, [employee.first_name, employee.last_name, employee.role_id, employee.manager_id], (error, result) => {
+                            if (error) throw error;
+                            console.log('\nEmployee added successfully!\n');
+                            resolve(result);
+                        });
+                    })
+                } catch {error} {
+                    console.error('Next')
+                }
                 break;
             case 'Update Employee Role':
                 console.log('\ncode for updating emp role')
                 break;
-            case 'View All Roles':
+            case 'View All Roles': //fin
                 console.log('\ncode for view allrolls')
-                db.query('SELECT * FROM role', (error, results) => {
-                    if (error) throw error;
-                    console.log('\n');
+                try {
+                    const results = await new Promise((resolve, reject) => {
+                        db.query('SELECT * FROM role', (error, results) => {
+                            if (error) reject(error);
+                            resolve(results);
+                        });
+                    });
+                    console.log('\n Printing Table: \n');
                     console.table(results);
-                });
-                break
-            case 'Add Role':
-                console.log('\ncode for adding role')
+                    console.log(`\n`);
+                } catch (error) {
+                    console.log('Error:', error);
+                }
                 break;
-            case 'View All Departments':
+            case 'Add Role': //fin
+                console.log('\nAdd a Role\n');
+                const newRole = await inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'title',
+                        message: 'Please enter role title: ',
+                    },
+                    {
+                        type: 'input',
+                        name: 'salary',
+                        message: 'Please input salary amount: ',
+                    },
+                    {
+                        type: 'input',
+                        name: 'department_id',
+                        message: 'Please input department id # for the new role',
+                    }
+                ]);
+                try {
+                    const result = await db.promise().query(
+                        'INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)',
+                        [newRole.title, newRole.salary, newRole.department_id]
+                    );
+                    console.log(`\n${result[0].affectedRows} role added!\n`)
+                } catch (error) {
+                    console.log(error);
+                }
+                break;
+            case 'View All Departments': // fin
                 console.log('\ncode for viewing all Departments')
-                db.query('SELECT * FROM department', (error, results) => {
-                    if (error) throw error;
-                    console.log('\n');
+                try {
+                    const results = await new Promise((resolve, reject) => {
+                        db.query('SELECT * FROM department', (error, results) => {
+                            if (error) reject(error);
+                            resolve(results);
+                        });
+                    });
+                    console.log('\n Printing Table: \n');
                     console.table(results);
-                });
+                    console.log(`\n`);
+                } catch (error) {
+                    console.log('Error:', error);
+                }
                 break;
-            case 'Add Department':
-                console.log('\ncode for adding Departments')
+            case 'Add Department':// fin
+                console.log('\nAdd Department\n');
+                const newDept = await inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'name',
+                        message: 'Enter new department name',
+                    }
+                ]);
+                try {
+                    const result = await db.promise().query('INSERT INTO department (name) VALUES (?)', [newDept.name]);
+                    console.log(`\nNew department: ${newDept.name} added`);
+                } catch (error) {
+                    console.error(`\n Next`);
+                }
                 break;
-            case 'Exit':
+            case 'Exit'://fin
                 // Exit the loop and the program
                 console.log('\nExiting program');
-                return process.exit(0);
+                isRun = false;
+                process.exit(0)
             default:
                 console.log('\nInvalid option\n');
                 break;
